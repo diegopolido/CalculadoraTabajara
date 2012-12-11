80f5090f339e64aaf1a56ef6ad2d9a1158ec6270
ios3135 <ios3135@Mac-mini-de-Caelum-36.local>
2012-12-11 18:50:28 -0200

    First commit

--- CalculadoraTabajara/ViewController.h
+++ CalculadoraTabajara/ViewController.h
@@ -9,5 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+@property (nonatomic, weak) IBOutlet UITextField * txtNumero1;
+@property (nonatomic, weak) IBOutlet UITextField * txtNumero2;
+@property (nonatomic, weak) IBOutlet UILabel * lblResultado;
+-(IBAction)somar:(UIButton*)botao;
+-(IBAction)selecionarValor1:(UISlider*)slider;
+-(IBAction)selecionarValor2:(UISlider*)slider;
 @end
--- CalculadoraTabajara/ViewController.m
+++ CalculadoraTabajara/ViewController.m
@@ -13,22 +13,20 @@
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+@synthesize txtNumero1, txtNumero2, lblResultado;
+-(IBAction)somar:(UIButton*)botao {
+    int num1 = [[txtNumero1 text] intValue];
+    int num2 = [[txtNumero2 text] intValue];
+    int resultado = num1 + num2;
+    
+    [lblResultado setText:[NSString stringWithFormat:@"%d", resultado]];
 }
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+-(IBAction)selecionarValor1:(UISlider*)slider {
+    int value = (int) slider.value;
+    txtNumero1.text = [NSString stringWithFormat:@"%d", value];
 }
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+-(IBAction)selecionarValor2:(UISlider*)slider {
+    int value = (int) slider.value;
+    txtNumero2.text = [NSString stringWithFormat:@"%d", value];
 }
-
 @end
