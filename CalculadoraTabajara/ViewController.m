02074e5302d4dbe3d1fda032787caf2a2bb0d21b
ios3135 <ios3135@Mac-mini-de-Caelum-36.local>
2012-12-11 19:03:01 -0200

    Sum float values

--- CalculadoraTabajara/ViewController.m
+++ CalculadoraTabajara/ViewController.m
@@ -15,18 +15,16 @@
 @implementation ViewController
 @synthesize txtNumero1, txtNumero2, lblResultado;
 -(IBAction)somar:(UIButton*)botao {
-    int num1 = [[txtNumero1 text] intValue];
-    int num2 = [[txtNumero2 text] intValue];
-    int resultado = num1 + num2;
+    float num1 = [[txtNumero1 text] floatValue];
+    float num2 = [[txtNumero2 text] floatValue];
+    float resultado = num1 + num2;
     
-    [lblResultado setText:[NSString stringWithFormat:@"%d", resultado]];
+    [lblResultado setText:[NSString stringWithFormat:@"%f", resultado]];
 }
 -(IBAction)selecionarValor1:(UISlider*)slider {
-    int value = (int) slider.value;
-    txtNumero1.text = [NSString stringWithFormat:@"%d", value];
+    txtNumero1.text = [NSString stringWithFormat:@"%f", slider.value];
 }
 -(IBAction)selecionarValor2:(UISlider*)slider {
-    int value = (int) slider.value;
-    txtNumero2.text = [NSString stringWithFormat:@"%d", value];
+    txtNumero2.text = [NSString stringWithFormat:@"%f", slider.value];
 }
 @end
